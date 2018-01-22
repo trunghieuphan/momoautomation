9d04ab821f3c3f200781f528afccc7532d5a7132
trunghieuphan <phantrunghieu.0194@gmail.com>
2018-01-22 15:22:49 +0700

    Fix bug Listener

--- framework/MomoAutomation/Libraries/Listeners/MomoAppiumLibraryListener.py
+++ framework/MomoAutomation/Libraries/Listeners/MomoAppiumLibraryListener.py
@@ -3,8 +3,8 @@ from robot.libraries.BuiltIn import BuiltIn
 
 ROBOT_LISTENER_API_VERSION = 2
 
-def end_keyword(name, attrs):
+def end_keyword(name, attrs): 
     if attrs['status'] == 'FAIL':
         for arg in attrs['args']:
             if(arg.startswith('${') and arg.endswith('}')):
-                logger.info(arg + ' = ' + BuiltIn().get_variable_value(arg))
+                logger.info(arg + ' = ' + str(BuiltIn().get_variable_value(arg)))
